68a451809e1fb31da179f1717c0ef58d71095d4f
Luis Luciano <alberto.rammss@gmail.com>
2020-12-02 14:14:59 -0600

    Update gitignore

--- output/consLet.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-var name = 'Luis Luciano morales';
-console.log('Hello world' + name);
--- output/templateStrings.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var name = "Luis";
-var age = 26;
-var country = "México";
-console.log("La persona es: ".concat(name, " tiene edad de: ").concat(age, " de ").concat(country));
